5955d70847d6468317df0851f2dacc6b06ee20e1
PanXiebit <ftdpanxie@gmail.com>
2021-12-15 21:42:36 +0800

    二叉树遍历

---
+++ leetcodes/112.路径总和.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode.cn id=112 lang=python3
+#
+# [112] 路径总和
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        self.sum = 0
+        return self.traserval(root, targetSum)
+
+    def traserval(self, cur, targetSum):
+        if cur is None:
+            return False
+        self.sum += cur.val
+
+        if cur.left is None and cur.right is None:
+            if self.sum == targetSum: return True
+            self.sum -= cur.val
+            return False
+        
+        if self.traserval(cur.left, targetSum): return True
+        if self.traserval(cur.right, targetSum): return True
+        self.sum -= cur.val
+        return False
+                
+# @lc code=end
+
