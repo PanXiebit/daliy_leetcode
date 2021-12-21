eed77e9d8f9dba118b3ceb474efa5736d6dd1ab0
PanXiebit <ftdpanxie@gmail.com>
2021-12-21 14:15:15 +0800

    二叉树递归

---
+++ leetcodes/654.最大二叉树.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=654 lang=python3
+#
+# [654] 最大二叉树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        if len(nums) == 0:
+            return
+        max_num = max(nums)
+        idx = nums.index(max_num)
+        root = TreeNode(max_num)
+        root.left = self.constructMaximumBinaryTree(nums[:idx])
+        root.right = self.constructMaximumBinaryTree(nums[idx+1:])
+
+        return root
+
+# @lc code=end
+
