7fa1d2b76644b9198687e2b06887e23d1ea34e0a
PanXiebit <ftdpanxie@gmail.com>
2021-12-21 13:13:22 +0800

    单调栈

--- leetcodes/739.每日温度.py
+++ leetcodes/739.每日温度.py
@@ -18,6 +18,7 @@ class Solution:
                     res[top_idx] = cur_idx - top_idx
                 
                 stack.append((cur_t, cur_idx))
+        print(stack)
         return res
 
 
