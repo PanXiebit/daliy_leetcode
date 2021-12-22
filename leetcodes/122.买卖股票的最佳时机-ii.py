d48d8c060b637dfd9740becabb6c359c04d09ea0
PanXiebit <ftdpanxie@gmail.com>
2021-12-22 22:16:20 +0800

    贪心， dp

---
+++ leetcodes/122.买卖股票的最佳时机-ii.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode.cn id=122 lang=python3
+#
+# [122] 买卖股票的最佳时机 II
+#
+
+# @lc code=start
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # 贪心
+        # res = 0
+        # for i in range(1, len(prices)):
+        #     if prices[i] > prices[i-1]:
+        #         res += prices[i] - prices[i-1]
+        # return res
+
+        # dp
+        dp = [[0, 0] for _ in range(len(prices))]
+        dp[0][0] = -prices[0]
+        dp[0][1] = 0
+        for i in range(1, len(prices)):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i])
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i])
+        print(dp)
+        return max(dp[-1][0], dp[-1][1])
+
+
+# @lc code=end
+
