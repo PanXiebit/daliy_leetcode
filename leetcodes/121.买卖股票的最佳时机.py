89683ea399925290f0c2d63ee26b65d9c8c93f90
PanXiebit <ftdpanxie@gmail.com>
2021-12-25 22:39:04 +0800

    股票问题

--- leetcodes/121.买卖股票的最佳时机.py
+++ leetcodes/121.买卖股票的最佳时机.py
@@ -15,16 +15,26 @@ class Solution:
         # return res
 
         # dp
-        dp = [[0, 0] for _ in range(len(prices))]
+        # dp = [[0, 0] for _ in range(len(prices))]
 
-        dp[0][0] = -prices[0]
-        dp[0][1] = 0
+        # dp[0][0] = -prices[0]
+        # dp[0][1] = 0
+
+        # for i in range(1, len(prices)):
+        #     # dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i])
+        #     dp[i][0] = max(dp[i-1][0], 0 - prices[i]) # 只能买入一次
+        #     dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i])
+        # return max(dp[-1][0], dp[-1][1])
+
+        # dp2
+        on = -prices[0]
+        out = 0
 
         for i in range(1, len(prices)):
-            # dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i])
-            dp[i][0] = max(dp[i-1][0], 0 - prices[i]) # 只能买入一次
-            dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i])
-        return max(dp[-1][0], dp[-1][1])
+            on = max(on, 0-prices[i])
+            out = max(out, on + prices[i])
+
+        return out
 
 # @lc code=end
 
