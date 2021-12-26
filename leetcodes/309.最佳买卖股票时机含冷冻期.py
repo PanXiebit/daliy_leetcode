354db2a3494c91ae669f5fe21f87ee8c537b3dd4
PanXiebit <ftdpanxie@gmail.com>
2021-12-26 15:27:15 +0800

    股票, dp

--- leetcodes/309.最佳买卖股票时机含冷冻期.py
+++ leetcodes/309.最佳买卖股票时机含冷冻期.py
@@ -17,19 +17,34 @@ class Solution:
         #     unhold = max(unhold, hold + prices[i])
         # return unhold 
 
-        dp = [[0,0] for _ in range(len(prices))]
+        # dp = [[0,0] for _ in range(len(prices))]
 
-        dp[0][0] = 0
-        dp[0][1] = -prices[0]
+        # dp[0][0] = 0
+        # dp[0][1] = -prices[0]
 
-        dp[1][0] = max(0, -prices[0] + prices[1])
-        dp[1][1] = max(-prices[0], 0 - prices[1])
+        # dp[1][0] = max(0, -prices[0] + prices[1])
+        # dp[1][1] = max(-prices[0], 0 - prices[1])
 
-        for i in range(1, len(prices)):
-            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]) # 不持有股票
-            dp[i][1] = max(dp[i-1][1], dp[i-2][0] - prices[i]) # 持有股票
+        # for i in range(1, len(prices)):
+        #     dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]) # 不持有股票
+        #     dp[i][1] = max(dp[i-1][1], dp[i-2][0] - prices[i]) # 持有股票
 
-        return dp[-1][0]
+        # return dp[-1][0]
+
+        # dp = [[0,0] for _ in range(len(prices))]
+
+        hold = -prices[0]
+        unhold = 0
+
+        pre_unhold = 0
+
+        for i in range(len(prices)):
+            tmp = unhold
+            unhold = max(unhold, hold + prices[i])
+            hold = max(hold, pre_unhold - prices[i])
+            pre_unhold = tmp
+
+        return unhold
 
 # @lc code=end
 
